3adb3fae0f901a906c701fa97feb6b618ec55eee
jmsmg <jmsmg1@me.com>
2021-11-23 17:36:30 +0900

    json 추출완료

--- telebot.py
+++ telebot.py
@@ -2,41 +2,36 @@ import http.client # HTTP 프로토콜의 클라이언트 역할
 import json
 import os # lambda 환경변수
 
-# 변수 설정 시작 #
 
+# 전역변수 설정 시작 #
 TELEGRAM_API_HOST = 'api.telegram.org' # 호스트 주소
 TOKEN = os.environ['TOKEN']
-
-# 변수 설정 끝 #
-
-## 호스트 주소 접속 객체 생성 ##
-connection = http.client.HTTPSConnection(TELEGRAM_API_HOST)
-
-# sendmessage 함수 url #
-url = f'/bot{TOKEN}'
-
+URL = f'/bot{TOKEN}'
 headers = {'content-type' : 'application/json'}
+connection = http.client.HTTPSConnection(TELEGRAM_API_HOST) # 호스트 주소 접속 객체 생성
+# 전역변수 설정 끝 #
 
-# 파라미터 #
-param = {
-    'chat_id' : os.environ['CHAT_ID'],
-    'text' : 'hello'
-}
+def lambda_handler(event, context):
 
-# http #
+    request_body = json.loads(event['body'])
+    # 파라미터 #
+    param = {
+        'chat_id' : os.environ['CHAT_ID'],
+        'text' : request_body['message']['text']
+    }
 
-## 요청 ##
-connection.request('POST', f'{url}/sendMessage', json.dumps(param) , headers)
-## 요청 끝 ##
+    ## 요청 ##
+    connection.request('POST', f'{URL}/sendMessage', json.dumps(param) , headers)
+    ## 요청 끝 ##
 
-## 응답 ##
-res = connection.getresponse()
-## 응답 끝 ##
+    ## 응답 ##
+    res = connection.getresponse()
+    ## 응답 끝 ##
 
-## response body 출력 ##
-print(json.dumps(json.loads(res.read().decode()), indent=4))
-print('상태코드 : ', res.status)
-print('메시지 : ', res.msg)
-
-# 강제 연결 종료 (비정상적인 요청 대비)#
-connection.close()
+    # 강제 연결 종료 (비정상적인 요청 대비)#
+    connection.close()
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
