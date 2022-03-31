6cdb5f86e2d71533b8694263ca28d2dc0ec355c0
jmsmg <jmsmg1@me.com>
2022-03-31 22:16:15 +0900

    api 응답부분 ft_response로 병합 완료

--- lambda_function.py
+++ lambda_function.py
@@ -5,11 +5,6 @@
 import http.client # HTTP 프로토콜의 클라이언트 역할
 import json
 import os
-<<<<<<< HEAD
-
-TOKEN = os.environ['TOKEN']
-URL = f'/bot{TOKEN}'
-=======
 
 _TELEGRAM_API_HOST = "api.telegram.org" # 호스트 주소
 _HEADERS = {'content-type' : 'application/json'}
@@ -23,7 +18,7 @@ class Bot:
         self._URL = URL
         self._command = command
 
-    def send_message(self) -> None:
+    def ft_response(self, respense) -> None:
         """
         명령어에 맞춰서 문자를 반환하는 함수
         """
@@ -31,46 +26,10 @@ class Bot:
         with open(f'param{self._command}.json') as f:
             param = json.loads(f.read())
 
-        _CONNECTION.request('POST', f'{self._URL}/sendMessage', json.dumps(param) , _HEADERS)
+        _CONNECTION.request('POST', f'{self._URL}{respense}', json.dumps(param) , _HEADERS)
 
         # 응답
         res = _CONNECTION.getresponse()
             
         # 강제 연결 종료 (비정상적인 요청 대비)#
         _CONNECTION.close()
->>>>>>> dda0057abdad278a7412967285c82b924d9f0e6a
-
-    def send_photo(self) -> None:
-        """
-        명령어에 맞춰서 사진 반환하는 함수
-        """
-        with open(f'param{self._command}.json') as f:
-            param = json.loads(f.read())
-
-
-<<<<<<< HEAD
-    tele_request = json.loads(event['body']) # 사용자에게서 들어오는 event['body']
-    command = tele_request["message"]["text"]
-    print(command)
-
-    bot = Bot(URL, command)
-
-    if command == '/table' or command == '/task' :
-        bot.ft_response("/sendMessage")
-    
-    elif command == '/photo':
-        bot.ft_response("/sendPhoto")
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
-=======
-        _CONNECTION.request('POST', f'{self._URL}/sendPhoto', json.dumps(param) , _HEADERS)
-
-        # 응답
-        res = _CONNECTION.getresponse()
-
-        # 강제 연결 종료 (비정상적인 요청 대비)#
-        _CONNECTION.close()
->>>>>>> dda0057abdad278a7412967285c82b924d9f0e6a
--- telebot.py
+++ telebot.py
@@ -18,40 +18,15 @@ class Bot:
         self._URL = URL
         self._command = command
 
-<<<<<<< HEAD
     def ft_response(self, respense) -> None:
-=======
-    def send_message(self) -> None:
->>>>>>> dda0057abdad278a7412967285c82b924d9f0e6a
         """
         명령어에 맞춰서 문자를 반환하는 함수
         """
 
         with open(f'param{self._command}.json') as f:
             param = json.loads(f.read())
-<<<<<<< HEAD
 
         _CONNECTION.request('POST', f'{self._URL}{respense}', json.dumps(param) , _HEADERS)
-=======
-
-        _CONNECTION.request('POST', f'{self._URL}/sendMessage', json.dumps(param) , _HEADERS)
-
-        # 응답
-        res = _CONNECTION.getresponse()
-            
-        # 강제 연결 종료 (비정상적인 요청 대비)#
-        _CONNECTION.close()
-
-    def send_photo(self) -> None:
-        """
-        명령어에 맞춰서 사진 반환하는 함수
-        """
-        with open(f'param{self._command}.json') as f:
-            param = json.loads(f.read())
-
-
-        _CONNECTION.request('POST', f'{self._URL}/sendPhoto', json.dumps(param) , _HEADERS)
->>>>>>> dda0057abdad278a7412967285c82b924d9f0e6a
 
         # 응답
         res = _CONNECTION.getresponse()
