dda0057abdad278a7412967285c82b924d9f0e6a
jmsmg <jmsmg1@me.com>
2022-03-31 16:10:54 +0900

    단톡방 버그 수정 -> 리팩토링 필요

--- telebot.py
+++ telebot.py
@@ -7,49 +7,42 @@ import json
 import os
 
 _TELEGRAM_API_HOST = "api.telegram.org" # 호스트 주소
-_TOKEN = os.environ['TOKEN']
-_URL = f'/bot{_TOKEN}'
 _HEADERS = {'content-type' : 'application/json'}
 _CONNECTION = http.client.HTTPSConnection(_TELEGRAM_API_HOST) # 호스트 주소 접속 객체 생성
 
 class Bot:
-    def __init__(self):
+    def __init__(self, URL, command):
         """
         인스턴스 초기화
         """
-        self._URL = _URL
-    
-    def get_command(self, command:str) -> None:
+        self._URL = URL
+        self._command = command
 
-        with open(f'param{command}.json') as f:
-            param = json.loads(f.read())
-        
-            if command == '/table':
-                self.send_message(param)
-        
-            elif command == '/photo':
-                self.send_photo(param)
-
-    def send_message(self, param) -> None:
+    def send_message(self) -> None:
         """
         명령어에 맞춰서 문자를 반환하는 함수
         """
 
-        _CONNECTION.request('POST', f'{_URL}/sendMessage', json.dumps(param) , _HEADERS)
+        with open(f'param{self._command}.json') as f:
+            param = json.loads(f.read())
+
+        _CONNECTION.request('POST', f'{self._URL}/sendMessage', json.dumps(param) , _HEADERS)
 
         # 응답
         res = _CONNECTION.getresponse()
-
+            
         # 강제 연결 종료 (비정상적인 요청 대비)#
         _CONNECTION.close()
 
-
-    def send_photo(self, param) -> None:
+    def send_photo(self) -> None:
         """
         명령어에 맞춰서 사진 반환하는 함수
         """
+        with open(f'param{self._command}.json') as f:
+            param = json.loads(f.read())
 
-        _CONNECTION.request('POST', f'{_URL}/sendPhoto', json.dumps(param) , _HEADERS)
+
+        _CONNECTION.request('POST', f'{self._URL}/sendPhoto', json.dumps(param) , _HEADERS)
 
         # 응답
         res = _CONNECTION.getresponse()
