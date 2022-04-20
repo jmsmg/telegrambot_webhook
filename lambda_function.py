b9a170ef50402a94efea9efa6f32670bfc3ef6b4
jmsmg <jmsmg1@me.com>
2022-04-20 23:45:24 +0000

    get

--- lambda_function.py
+++ lambda_function.py
@@ -15,17 +15,27 @@ def lambda_handler(event, context):
     print("context :", context) # 들어오는 값 확인용
     
     tele_request = json.loads(event['body']) # 사용자에게서 들어오는 event['body']
-    command = tele_request["message"]["text"]
-    sender_name = tele_request["message"]["from"]["first_name"]
+    print(tele_request)
+
+    # json 문법 체크
+    if tele_request.get('callback_query'): # 버튼 눌렀을때
+        data = tele_request["callback_query"]["data"]
+        message_id = tele_request["callback_query"]["message"]["message_id"]
+        sender_name = tele_request["callback_query"]["from"]["first_name"]
+        command = f'button_data_{data}'
+
+    elif tele_request.get('message'): # 메세지를 받았을때
+        command = tele_request["message"]["text"]
+        sender_name = tele_request["message"]["from"]["first_name"]
+        
+    bot = Bot(URL)
     
-    bot = Bot(URL, command)
-
-    method = bot.find_method()
-
-    bot.ft_response(method, sender_name)
-
+    api_method = bot.find_method(command)
+    bot.ft_response(api_method, sender_name, command)
+    # 콜백 쿼리 부분
 
     # DB 연결 부분
+    
     connect_db = Connect_db()
     print(connect_db.read_table())
     
--- telebot.py
+++ telebot.py
@@ -4,6 +4,7 @@
 
 import http.client # HTTP 프로토콜의 클라이언트 역할
 import json
+import re
 
 _TELEGRAM_API_HOST = "api.telegram.org" # 호스트 주소
 _HEADERS = {'content-type' : 'application/json'}
@@ -11,36 +12,47 @@ _CONNECTION = http.client.HTTPSConnection(_TELEGRAM_API_HOST) # 호스트 주소
 
 class Bot:
 
-    def __init__(self, URL, command):
+    def __init__(self, URL):
         """
         인스턴스 초기화
         """
         self._URL = URL
-        self._command = command
-    
-    def find_method(self):
-        """
-        명령어를 받아 그에 맞는 메소드를 찾아주는 함수
-        """
-        
-        if self._command == '/table' or self._command == '/task' :
-            return '/sendMessage'    
-        elif self._command == '/photo':
+
+    def find_method(self, command):
+        if command == '/table' or command == '/task' :
+            return '/sendMessage'
+
+        elif command == '/photo':
             return '/sendPhoto'
 
-    def ft_response(self, method, sender_name) -> None:
+        else: # 정규표현식으로 변경해야함
+            return '/editMessageText'
+
+    # def check_data(self, data):
+    #     if data == '0'
+    #         pass 
+    #     elif data == 'vi'
+    #         pass
+    #     elif data == 'bo'
+    #         pass
+    #     elif data == 'st'
+    #         pass
+    #     elif data == 'ba'
+    #         pass
+    
+    def ft_response(self, response, sender_name, command) -> None:
         """
-        메소드에 맞춰서 문자를 반환하는 함수
+        명령어에 맞춰서 문자를 반환하는 함수
         """
 
-        with open(f'param{self._command}.json') as f:
+        with open(f'param{command}.json') as f:
             param = json.loads(f.read())
             param["text"] = f"호출자 : {sender_name}"
 
-        _CONNECTION.request('POST', f'{self._URL}{method}', json.dumps(param) , _HEADERS)
+        _CONNECTION.request('POST', f'{self._URL}{response}', json.dumps(param) , _HEADERS)
 
         # 응답
-        # res = _CONNECTION.getresponse()
+        res = _CONNECTION.getresponse()
 
         # 강제 연결 종료 (비정상적인 요청 대비)#
         _CONNECTION.close()
